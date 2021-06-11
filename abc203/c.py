610f977967afeebc04b0646e291d7c3898ca6539
Tatsuya Nagashima <>
2021-06-12 07:48:09 +0900

    Add:abc203 c

--- abc203/c.py
+++ abc203/c.py
@@ -1,2 +1,12 @@
-N,K = map(int, input().split())
-ab = [list(input()) for _ in range(N)]
+N,K=map(int,input().split())
+AB = [list(map(int,input().split())) for _ in range(N)]
+AB.sort()
+
+ans = K
+for i in AB:
+  if i[0]<=K:
+    K+=i[1]
+  else:
+    break
+  
+print(K) 
